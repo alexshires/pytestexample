44d59840b530bef277b6d6b030c61bf1c9514786
Alex <ashires@deloitte.co.uk>
2020-06-02 15:53:35 +0100

    init

---
+++ src/example.py
@@ -0,0 +1,24 @@
+"""
+example function to test
+"""
+
+
+def function_to_test():
+    """
+    example
+    :return:
+    """
+    return "Hello World"
+
+
+class ClassToTest(object):
+
+    def __init__(self):
+        pass
+
+    def class_function(self):
+        """
+        test
+        :return:
+        """
+        return "class function"
---
+++ test/test_example.py
@@ -0,0 +1,50 @@
+"""
+Pytest file
+"""
+
+import json
+
+import pytest
+
+from src.example import ClassToTest
+from src.example import function_to_test
+
+DATA_FILE = "test/data/data.json"
+
+
+def test_example():
+    """
+    test the function
+    :return:
+    """
+    res = function_to_test()
+    assert (res == "Hello World")
+
+
+def test_class():
+    """
+    test class
+    :return:
+    """
+    c = ClassToTest()
+    res = c.class_function()
+    assert (res == "class function")
+
+
+@pytest.mark.parametrize("input_val,output_val", ([1, 2], [2, 3]))
+def test_to_param(input_val, output_val):
+    """
+    test params
+    :return:
+    """
+    assert input_val + 1 == output_val
+
+
+def test_data():
+    """
+    test data file
+    :return:
+    """
+    with open(DATA_FILE) as f:
+        data_json = json.load(f)
+        assert (True)
