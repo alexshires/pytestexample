286bd7ecce934f3421ccdadc5aed17ccc91c2144
Alex <ashires@deloitte.co.uk>
2020-06-02 18:06:42 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 Define Categorise Module
 """
 # from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="testdemo",
     version="0.1",
-    packages=["src",],
+    packages=find_packages(),
     license="",
     long_description=open("README.md").read(),
 )
